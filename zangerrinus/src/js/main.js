b4a2fcdb7e3927175743c3e276d353c8b343f3e8
Manaus Transez <manatran@student.arteveldehs.be>
2018-10-19 17:19:33 +0200

    Added some custom css

--- zangerrinus/src/js/main.js
+++ zangerrinus/src/js/main.js
@@ -1,3 +1,31 @@
 if(true){
-  console.log('test')
+  console.log(
+    " -yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy-\n",
+    "-yyyyyyyyyyyyyyyyyys    syyyyyyyyyyyyyyyyyy-\n",
+    "-MMMMMMMMMMMMMMM           NMMMMMMMMMMMMMMM-\n",
+    "-MMMMMMMMMMMN        _         MMMMMMMMMMMM-\n",
+    "-MMMMMMMMmhs      shmMNmy       ydNMMMMMMMM-\n",
+    "-MMMMNdy       ydNN      NNhs      shmMMMMM-\n",
+    "-Mmhs      shmN             NNdy       ydNM-\n",
+    "-       ydNNdy                ymNNds      s-\n",
+    "-    hNMmhs                      shNMmy    -\n",
+    "-   sMN      ssssss      ssssss     sMm    -\n",
+    "-   sMm      MMMMMMm    mMMMMMM      Mm    -\n",
+    "-   sMm      MMMMMMMMMMMMMMMMMM      Mm    -\n",
+    "-   sMm      MMMMMMMMMMMMMMMMMM      Mm    -\n",
+    "-   sMm      MMMN          NMMM      Mm    -\n",
+    "-   sMm      MMMMMMM    MMMMMMM      Mm    -\n",
+    "-   sMm      MMMMMMM    MMMMMMM      Mm    -\n",
+    "-   sMm      MMMMMMM    MMMMMMM      Mm    -\n",
+    "-   sMm      MMMMMMM    MMMMMMM      Mm    -\n",
+    "-    dNNdy                        ymMNh    -\n",
+    "-      shNMmh                 shNMdy       -\n",
+    "-Ndy       hdNNds          ymNNds       ydN-\n",
+    "-MMMMmh       shmMmys  shNMmy       shmMMMM-\n",
+    "-MMMMMMMNds       ymMNMNhs       ymMMMMMMMM-\n",
+    "-MMMMMMMMMMMmys      ^       shNMMMMMMMMMMM-\n",
+    "-MMMMMMMMMMMMMMNdy        ydNMMMMMMMMMMMMMM-\n",
+    "-ddddddddddddddddddy///shdddddddddddddddddd-\n",
+    "-dddddddddddddddddddddddddddddddddddddddddd-\n",
+  );
 }
